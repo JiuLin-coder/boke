4c3242c799fc6009607c2086bce5401eecc053c9
JiuLin <JiuLin1217@outlook.com>
2026-04-21 23:33:49 +0800

    fix:money.vue

--- src/pages/money.vue
+++ src/pages/money.vue
@@ -9,9 +9,9 @@
         <div></div>
         <!-- 每日 -->
         <div class="flex column">
-          <div v-for="item in dayFreeList" :key="item" class="flex">
-            <div>{{ item }}</div>
-            <div>nieRong</div>
+          <div v-for="item in manyTime" :key="item[2]" class="flex">
+            <div>{{ item[2] }}</div>
+            <div>{{ item[3] }}</div>
             <button v-on:click="updateDayFrees(item)">修改</button>
           </div>
         </div>
@@ -23,12 +23,16 @@
       <button v-on:click="nextMonth(1)">下一月</button>
 
       <hr />
-      date: {{ aDay[0] }} |everyFree {{ aDay[1] }} | sum:{{ aDay[2] }}
-      <input type="text" v-model="everyFree" placeholder="每次输入一个值" />
+      {{ everyFrees }}
+      <input
+        type="text"
+        v-model.number="everyFree"
+        placeholder="每次输入一个值"
+      />
       <button v-on:click="addFree()">增加</button>
-      <button v-on:click="updateMaryDay()">缓存</button>
-      <div>已缓存数据条：</div>
-      <div>{{ maryDay }}</div>
+      <button v-on:click="temporaryManyTime()">缓存</button>
+      <div>已缓存数据条：{{ temporaryManyTimeList }}</div>
+      <div></div>
 
       <hr />
       <button v-on:click="publish()">上传</button>
@@ -38,120 +42,108 @@
 
 <style></style>
 
-<script setup>
-//前端
-import { ref } from "vue";
-//初始化当前年月
-const year = ref(new Date().getFullYear());
-const month = ref(new Date().getMonth() + 1);
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+      month: new Date().getMonth() + 1,
+      timeFreeList: [], //设置一个数据条:year,month,day,[free],sum
+      manyTime: [], //展示的当前年月所有数据条
+      everyFrees: [],
+      everyFree: null,
+      temporaryManyTimeList: [], //缓存前端已修改的数据条
+    };
+  },
+  mounted() {
+    //初始化当前日
+    this.createDayList();
+  },
+  methods: {
+    //数据条
+    createDayList() {
+      //创建数据条
+      this.timeFreeList = [this.year, this.month, 1, [], 0];
 
-//动态渲染日数组
-const dayFreeList = ref([]);
-function createDayList() {
-  dayFreeList.value = [];
-  //获取特定月的日数组
-  let maxDay = new Date(year.value, month.value, 0).getDate();
-  for (let iDays = 1; iDays <= maxDay; iDays++) {
-    dayFreeList.value.push(("0" + iDays).slice(-2));
-  }
-}
+      //获取特定月的日数组
+      let maxDay = new Date(this.year, this.month, 0).getDate();
+      for (let iDays = 1; iDays <= maxDay; iDays++) {
+        this.timeFreeList[2] = iDays;
+        this.timeFreeList[3] = []; //这么做，才能深拷贝
+        this.manyTime.push(this.timeFreeList.slice()); //.slice是为了实现数组的深拷贝，其实这种直接对象也是浅拷贝
+      }
+      this.timeFreeList = [];
+    },
 
-//初始化当前日
-createDayList();
-//动态改变年月
-function nextMonth(step) {
-  if (step) {
-    //下一月
-    if (month.value == 12) {
-      year.value += 1;
-      month.value = 0;
-    }
-    month.value += 1;
-  }
-  if (!step) {
-    //上一月
-    if (month.value == 1) {
-      year.value -= 1;
-      month.value = 13;
-    }
-    month.value -= 1;
-  }
-  createDayList();
-}
+    //动态改变年月
+    nextMonth(step) {
+      if (step) {
+        //下一月
+        if (this.month == 12) {
+          this.year += 1;
+          this.month = 0;
+        }
+        this.month += 1;
+      }
+      if (!step) {
+        //上一月
+        if (this.month == 1) {
+          this.year -= 1;
+          this.month = 13;
+        }
+        this.month -= 1;
+      }
+      //确保为该月
+      this.manyTime = [];
+      //重新获得日数组
+      this.createDayList();
+    },
 
-//缓存在前端已修改的所有数据条
-const maryDay = ref([]);
-//设置一个数据条
-const aDay = ref(["", [], 0]); //aDay= [dayDate,[],sum]
+    updateDayFrees(everyFrees) {
+      this.everyFrees = everyFrees.slice(); //设定特定的数据条，
+    },
 
-function updateDayFrees(day) {
-  //自动缓存，不过若是没有确认是之前没有添加，则不触发
-  if (aDay.value[0]) {
-    updateMaryDay();
-  }
-  //获得索引，年月日
-  aDay.value[0] = year.value + "-" + ("0" + month.value).slice(-2) + "-" + day;
+    addFree() {
+      this.everyFrees[3].push(this.everyFree); //增加free
+      this.everyFrees[4] += this.everyFree; //改变sum
 
-  //后面发现浏览器这里有个古怪的bug，当我点击相同的之后，会把之前缓存的那些拉回来
-  //但是若是我console然后第一次查看全部就是之前那个数，
-  //若我是等到第二个缓存好后再查看全部，结果发现第一个那条的结果改变了，与第二个一致。
-  //我初步判断是跟浅拷贝的原因。
-  // 不过后面我仍然会把第一条删掉，然后添加第二条，所以就没去验证是不是浅拷贝
-  //处理day相同的数据条
-  maryDay.value.map((item) => {
-    if (item[0] == aDay.value[0]) {
-      //把之前那条的数据给新一条
-      aDay.value = item;
-      //并删除之前那条
-      let newMaryDay = [];
-      maryDay.value.map((item1) => {
-        if (item1[0] !== aDay.value[0]) {
-          newMaryDay.push(item1);
+      this.everyFree = null;
+    },
+
+    //好像不用，可以直接上传
+    temporaryManyTime() {
+      let newManyTime = [];
+      this.manyTime.map((item) => {
+        //因为只能修改当前年月，所以年月肯定相等，日相等，说明修改是此日
+        if (item[2] == this.everyFrees[2]) {
+          newManyTime.push(this.everyFrees);
+        } else {
+          newManyTime.push(item);
         }
       });
-      maryDay.value = newMaryDay;
-    }
-  });
-}
 
-const everyFree = ref();
-function addFree() {
-  //若是用户没有选择day怎么办
-  if (!aDay.value[0]) {
-    alert("请选择哪一天");
-    return;
-  }
-  //从aDay第二项的元素增加每笔费用
-  aDay.value[1].push(everyFree.value);
-  //计算每日总和
-  aDay.value[2] = Number(aDay.value[2]) + Number(everyFree.value);
-  everyFree.value = null;
-}
+      this.manyTime = newManyTime;
+      this.everyFrees = [];
+    },
 
-function updateMaryDay() {
-  //把改变的数据条收集起来
-  maryDay.value.push(aDay.value);
-  //并清空aDay，使得不影响下一次增加dayFree
-  aDay.value = ["", [], 0];
-}
+    publish() {
+      //全部上传，然后到后端给整理好
+      //只上传有的
+      let publishManyTime = [];
+      this.manyTime.map((item) => {
+        if (item[3][0]) {
+          publishManyTime.push(item);
+        }
+      });
+      //现在只能先保存在localStorage
 
-function publish() {
-  //防止最后一个没有缓存
-  updateMaryDay();
-  //将没有free的剔除
-  let newMaryDay = [];
-  maryDay.value.map((item) => {
-    if (item[2]) {
-      newMaryDay.push(item);
-    }
-  });
-  maryDay.value = newMaryDay;
-  // maryDay.value;//网络通信上传数据条
-  // 不过没有后端，那就只能保存到本地了
+      //把temporaryManyTimeList给上传到后端
 
-  //清空maryDay
-  maryDay.value = [];
-}
-
-//后端
+      // //然后从保存来的一定是一大块
+      // window.localStorage.setItem("neirong", nieRong.value);
+      // window.localStorage.getItem("neirong");
+      // window.localStorage.removeItem("neirong");
+    },
+  },
+};
 </script>
