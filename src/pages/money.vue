fc7c8fb3ee4e2a1d129ac5f1cbb02d6060ceed4b
JiuLin <JiuLin1217@outlook.com>
2026-05-03 22:49:28 +0800

    修饰行为

--- src/pages/money.vue
+++ src/pages/money.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex1 flex">
     <!-- 展示月总和 -->
-    <div style="width: 20rem" class="shadow"></div>
+    <div style="width: 20rem" class="shadow">
+      <button v-on:click="clearDate()">清空账本</button>
+    </div>
     <!-- 展示月的每日数据 -->
     <div class="flex1 flex column mg40 fs16 shadow">
       <div class="flex1 flex jcc aic area">
@@ -70,105 +72,23 @@ export default {
       isMonth: false,
     };
   },
+
+  //初始化
   mounted() {
     //获取所有的账本数据
     this.date = window.localStorage.getItem("date")
       ? JSON.parse(window.localStorage.getItem("date"))
       : {};
 
-    this.createYear();
-    this.createMonth();
+    this.updateYear();
+    this.updateMonth();
 
     console.log(this.date);
-    console.log(this.everyFrees);
   },
+
   methods: {
-    //数据条
-    createDayList() {
-      //清空disMonth
-      this.disMonth = [];
-
-      //创建数据条
-      this.everyFrees = [1, []];
-
-      //获取特定月的日数组
-      let maxDay = new Date(this.year, this.month, 0).getDate();
-      for (let iDays = 1; iDays <= maxDay; iDays++) {
-        this.everyFrees[0] = iDays;
-        this.everyFrees[1] = [];
-        //如果说要在数组里嵌套数组，则必须先let a= []; a[0] = [] 这么做，才能深拷贝。
-        //动态问题，其实是深拷贝问题啊，毕竟深拷贝后，数组就是新数组，
-        //.slice是为了实现数组的深拷贝，因为直接引入(push)只是还是浅拷贝引入数组地址而言，最终多个嵌套共用一个数组，
-        //对象也是同理。
-        this.disMonth.push(this.everyFrees.slice());
-      }
-      this.everyFrees = [];
-    },
-
-    //动态改变年月，而年月也只会在缓存的时候使用
-    nextMonth(step) {
-      if (step) {
-        //下一月
-        if (this.month == 12) {
-          this.year += 1;
-          this.createYear();
-          this.month = 0;
-        }
-        this.month += 1;
-        this.createMonth();
-      }
-      if (!step) {
-        //上一月
-        if (this.month == 1) {
-          this.year -= 1;
-          this.createYear();
-          this.month = 13;
-        }
-        this.month -= 1;
-        this.createMonth();
-      }
-    },
-
-    getDayFrees(everyFrees) {
-      this.everyFrees = everyFrees; //设定特定的数据条，虽然数组指向没有变化
-    },
-
-    addFree() {
-      this.everyFrees[1].push(this.everyFree); //增加free
-      this.everyFree = null;
-      //因为数组地址指向没有变化，所以我不用把修改后的Frees重新删除复制回去。
-      //修改后的直接全保存在disMonth中，disMonth的作用就从展示数据，到获取和保存数据了
-    },
-
-    deleteDayFrees(everyFrees) {
-      this.everyFrees = everyFrees;
-      this.everyFrees[1] = [];
-      console.log(this.everyFrees);
-      delete this.date[this.year][this.month][everyFrees[0]]; //date也要删掉，但是这里的date是前端的，还是要点击缓存才能针对localStorage
-    },
-
-    //保存在localStorage
-    localPublish() {
-      //重新保存回date
-      this.disMonth.map((item) => {
-        //只上传有的
-        if (item[1][0]) {
-          //dismonth每一条的[1,[]]改成date.year.month的每一条{1：[]}。date:{year:{month:{day:[]}}}
-          //并且这个若元素重名，则会替代掉原来的元素，
-          Object.defineProperty(this.date[this.year][this.month], item[0], {
-            value: item[1].slice(),
-            writable: true,
-            enumerable: true,
-            configurable: true,
-          });
-        }
-      });
-      console.log(this.date);
-
-      window.localStorage.setItem("date", JSON.stringify(this.date)); //localStorage只保存字符串，//并且直接替代原date
-    },
-
-    createYear() {
+    //初始化date的年
+    updateYear() {
       if (this.date) {
         Object.keys(this.date).map((item) => {
           if (Number(item) == this.year) {
@@ -192,7 +112,8 @@ export default {
       this.isYear = false;
     },
 
-    createMonth() {
+    //初始化date的月
+    updateMonth() {
       this.createDayList(); //初始和切换要清空disMonth，不然会把上个月的带进去。
 
       Object.keys(this.date[this.year]).map((item) => {
@@ -225,6 +146,116 @@ export default {
       this.isMonth = false;
     },
 
+    //disMonth的日数据条
+    createDayList() {
+      //清空disMonth
+      this.disMonth = [];
+
+      //创建数据条
+      this.everyFrees = [1, []];
+
+      //获取特定月的日数组
+      let maxDay = new Date(this.year, this.month, 0).getDate();
+      for (let iDays = 1; iDays <= maxDay; iDays++) {
+        this.everyFrees[0] = iDays;
+        this.everyFrees[1] = [];
+        //如果说要在数组里嵌套数组，则必须先let a= []; a[0] = [] 这么做，才能深拷贝。
+        //动态问题，其实是深拷贝问题啊，毕竟深拷贝后，数组就是新数组，
+        //.slice是为了实现数组的深拷贝，因为直接引入(push)只是还是浅拷贝引入数组地址而言，最终多个嵌套共用一个数组，
+        //对象也是同理。
+        this.disMonth.push(this.everyFrees.slice());
+      }
+      this.everyFrees = [];
+    },
+
+    //动态改变年月，而年月也只会在缓存的时候使用
+    nextMonth(step) {
+      if (step) {
+        //下一月
+        if (this.month == 12) {
+          this.year += 1;
+          this.updateYear();
+          this.month = 0;
+        }
+        this.month += 1;
+        this.updateMonth();
+      }
+      if (!step) {
+        //上一月
+        if (this.month == 1) {
+          this.year -= 1;
+          this.updateYear();
+          this.month = 13;
+        }
+        this.month -= 1;
+        this.updateMonth();
+      }
+    },
+
+    //获得特定disMonth的数据条，虽然数组指向没有变化
+    getDayFrees(everyFrees) {
+      this.everyFrees = everyFrees;
+    },
+
+    //给disMonth的数据条增加数据
+    addFree() {
+      if (!this.everyFrees[0]) {
+        window.alert("请选择哪一天");
+        return;
+      }
+      if (!this.everyFree) {
+        window.alert("输入框为空");
+        return;
+      }
+
+      this.everyFrees[1].push(this.everyFree); //增加free
+      this.everyFree = null;
+      //因为数组地址指向没有变化，所以我不用把修改后的Frees重新删除复制回去。
+      //修改后的直接全保存在disMonth中，disMonth的作用就从展示数据，到获取和保存数据了
+    },
+
+    //清空对应的数据条
+    deleteDayFrees(everyFrees) {
+      if (!window.confirm("是否删除")) {
+        return;
+      }
+
+      this.everyFrees = everyFrees;
+      this.everyFrees[1] = [];
+      console.log(this.everyFrees);
+      delete this.date[this.year][this.month][everyFrees[0]]; //date也要删掉，但是这里的date是前端的，还是要点击缓存才能针对localStorage
+    },
+
+    //保存在localStorage
+    localPublish() {
+      //把disMonth的日数据条保存到date里
+      this.disMonth.map((item) => {
+        //只上传有的
+        if (item[1][0]) {
+          //dismonth每一条的[1,[]]改成date.year.month的每一条{1：[]}。date:{year:{month:{day:[]}}}
+          //并且这个若元素重名，则会替代掉原来的元素，
+          Object.defineProperty(this.date[this.year][this.month], item[0], {
+            value: item[1].slice(),
+            writable: true,
+            enumerable: true,
+            configurable: true,
+          });
+        }
+      });
+      console.log(this.date);
+
+      window.localStorage.setItem("date", JSON.stringify(this.date)); //localStorage只保存字符串，//并且直接替代原date
+    },
+
+    //清空本地缓存的账本
+    clearDate() {
+      if (!window.confirm("是否清空账本")) {
+        return;
+      }
+      window.localStorage.removeItem("date");
+      // 刷新当前页面
+      history.go(0);
+    },
     publish() {
       //把存储在localStorage里的上传到后端
       //若有后端，每次打开账本，把后端的发送到locaoStorage
