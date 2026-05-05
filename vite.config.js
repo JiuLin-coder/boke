c0f1728dec22db6f4b85dc263e9a1e0373c22887
JiuLin <JiuLin1217@outlook.com>
2026-05-05 13:45:39 +0800

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
 
-function pathResolve(dir) {
-  return resolve(__dirname, ".", dir);
-}
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": pathResolve("src"),
-    },
-  },
 });
