ed2edfaa9fe0d7218197067efb793b723aa4a4a6
JiuLin <JiuLin1217@outlook.com>
2026-05-07 22:00:38 +0800

    Fix 增强用户体验

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/boke/", // 关键！改为相对路径,/本地部署改成./  ，而github page要改成/boke/  （当然也可以统一改成/boke）
+  base: "/boke/", // 关键！改为相对路径,/本地部署改成./  ，而github page要改成/boke/  （当然也可以统一改成/boke/）
   build: {
     outDir: "dist",
     assetsDir: "src",
